2c17c75022e2b8dd1575c280586efc27895cdea9
Julian Lamadrid <jelamadrid@e-din.com.ar>
2014-03-11 17:47:25 -0300

    configurado bootstrap

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+UmSurveys::Application.config.secret_key_base = '9256087cc9a80ec7f6568f88fb6d15e08026006d25c884b49957506748e3a753b2818e9c5995a47eb88598db1b8fb6f4655636eb103c73f8dea42bbd482822e3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+UmSurveys::Application.config.session_store :cookie_store, key: '_um-surveys_session'
