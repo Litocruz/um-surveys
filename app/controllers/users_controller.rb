7146396365fed41b8e6a06839de2e6aedef35667
litocruz <rengolamadrid@hotmail.com>
2014-03-12 11:34:26 -0300

    Terminado usuarios, edit, update, index y destroy

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,12 @@
 class UsersController < ApplicationController
+  before_action :signed_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user, only: :destroy
+
+  def index
+    @users = User.paginate(page: params[:page])
+  end
+
   def new
     @user = User.new
   end
@@ -8,6 +16,7 @@ class UsersController < ApplicationController
   def edit
     @user = User.find(params[:id])
   end
+
   def update
     @user = User.find(params[:id])
     if @user.update_attributes(user_params)
@@ -29,8 +38,29 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "Usuario eliminado."
+    redirect_to users_url
+  end
+
   private
     def user_params
-      params.require(:user).permit(:name,:email,:password,:password_confirmation)
+     params.require(:user).permit(:name,:email,:password,:password_confirmation)
+    end
+
+  #before action
+    def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "Por favor inicie sesion."
+      end
+    end
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 UmSurveys::Application.routes.draw do
   resources :users
   resources :sessions, only: [:new, :create, :destroy]
-  root  'sessions#new'
+  root  'users#index'
   match '/signup',  to: 'users#new',            via: 'get'
   match '/signin',  to: 'sessions#new',            via: 'get'
   match '/signout',  to: 'sessions#destroy',            via: 'delete'
