c3a016551d339bb4414d528030c53ce38cda62c1
Julian Lamadrid <jelamadrid@e-din.com.ar>
2014-03-11 21:03:14 -0300

    editar usuarios

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,12 +5,24 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+  def edit
+    @user = User.find(params[:id])
+  end
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Perfil actualizado"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
 
   def create
     @user = User.new(user_params)
     if @user.save
       sign_in @user
-      flash[:success] = "Welcome to the Sample App!"
+      flash[:success] = "Bienvenido!"
       redirect_to @user
     else
       render 'new'
